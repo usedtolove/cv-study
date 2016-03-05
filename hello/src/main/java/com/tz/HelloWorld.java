da4ac41683792d54c28d803d29431e797b323ce9
usedtolove <usedtolove@gmail.com>
2016-03-05 16:36:44 +0800

    16:36

---
+++ hello/src/main/java/com/tz/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.tz;
+
+/**
+ * Created by hjl on 2016/3/5.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+//        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
+
+}
